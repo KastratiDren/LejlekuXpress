3f45ffb903f487fbb0c539c4787ef6c37a5b0888
KastratiDren <drenkastrati33@gmail.com>
2024-10-23 18:26:15 +0200

    created the database context

---
+++ LejlekuXpress/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LejlekuXpress.Data
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public AppDbContext(DbContextOptions<AppDbContext> options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+    }
+}
