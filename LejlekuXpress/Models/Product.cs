e83e952c6b956fcf971006ba6233dcd27970ece8
KastratiDren <drenkastrati33@gmail.com>
2024-10-25 19:06:59 +0200

    created the product model

---
+++ LejlekuXpress/Models/Product.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace LejlekuXpress.Models
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public bool IsApproved { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        public byte[] Image { get; set; }
+
+        public string Specifications { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [Required]
+        public bool IsReviewed { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal ShippingPrice { get; set; }
+    }
+}
