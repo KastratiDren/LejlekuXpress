1ad89dcf40de2657be076b6520b4811ca4c29954
KastratiDren <drenkastrati33@gmail.com>
2024-10-31 14:04:07 +0100

    adding services to the DI container

--- LejlekuXpress/Program.cs
+++ LejlekuXpress/Program.cs
@@ -1,12 +1,28 @@
 using LejlekuXpress.Data;
+using LejlekuXpress.Data.ServiceInterfaces;
+using LejlekuXpress.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IRolesService, RolesService>();
+builder.Services.AddScoped<ICountryService, CountryService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IShippingAddressService, ShippingAddressService>();
+builder.Services.AddScoped<IPaymentService, PaymentService>();
+builder.Services.AddScoped<IProductService, ProductService>();
+builder.Services.AddScoped<IWishlistService, WishlistService>();
+builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddScoped<ICheckOutService, CheckOutService>();
+builder.Services.AddScoped<IOrdersService, OrdersService>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
