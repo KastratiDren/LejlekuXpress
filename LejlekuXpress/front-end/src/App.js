e7d2186a52a9389bb48c70e903bba421753932c8
KastratiDren <drenkastrati33@gmail.com>
2024-11-07 18:48:21 +0100

    created the wishlist page

--- LejlekuXpress/front-end/src/App.js
+++ LejlekuXpress/front-end/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import {BrowserRouter, Routes,Route} from "react-router-dom";
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Login from './pages/Login';
+import ForgotPassword from './pages/ForgotPassword';
+import Signup from './pages/Signup';
+import OurStore from './pages/OurStore';
+import Wishlist from './pages/Wishlist';
+import SingleProduct from './pages/SingleProduct';
+import UserDashboard from './pages/UserDashboard';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
+import AdminDashboard from './pages/AdminDashboard/AdminDashboard';
+import TermsAndConditions from './pages/TermsAndConditions';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+//import useAuthToken from './components/useAuthToken.js';
+
 
 function App() {
+  // const token = useAuthToken();
+  // useEffect(() => {
+  //   console.log(token);
+  // }, [token]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/product" element={<OurStore />} />
+            <Route path="/product/:id" element={<SingleProduct />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/wishlist" element={<Wishlist />} />
+            <Route path="/userdashboard" element={<UserDashboard />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/admin" element={<AdminDashboard />} />
+            <Route path="/TermsAndConditions" element={<TermsAndConditions />} />
+            <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
