7197997ed428a1881ac26d12bff61cfec64bc1b4
KastratiDren <drenkastrati33@gmail.com>
2024-11-09 15:50:38 +0100

    creating the Log class

---
+++ LejlekuXpress/Models/Log.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace LejlekuXpress.Models
+{
+    public class Log
+    {
+        [BsonId]  
+        [BsonRepresentation(BsonType.ObjectId)] 
+        public ObjectId Id { get; set; }
+        public string Action { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+        public string Message { get; set; }
+    }
+
+}
